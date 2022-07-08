19e2fa0c98361f99ee8a5b17c7787988d1ea29dc
₸ornike <20404466+e-tornike@users.noreply.github.com>
2022-07-08 13:20:44 +0200

    convert from pytrec_eval to ranx

--- scripts/evaluate_task2.py
+++ scripts/evaluate_task2.py
@@ -1,36 +1,25 @@
-import pytrec_eval
+from ranx import evaluate
 import os
 import json
 from typing import Dict, Tuple, List
 from argparse import ArgumentParser
 
 
-DEFAULT_METRICS = ["map@20,50", "p@1,3,5,10,20", "r@1,3,5,10,20", "rprec"]
-PYTREC_METRIC_MAPPING = {"map": "map_cut", "rprec": "Rprec", "p": "P", "r": "recall"}
+DEFAULT_METRICS = ["map@10", "map@20", "map@30", "r-precision"]
 
 
 def _evaluate(
     qrels: Dict[str, Dict[str, int]], 
     results: Dict[str, Dict[str, float]], 
-    measures: Dict[str, str]
+    measures: List[str]
     ) -> Tuple[Dict[str, float], Dict[str, float], Dict[str, float], Dict[str, float]]:
         
-        evaluator = pytrec_eval.RelevanceEvaluator(qrels, [k for _,k in measures.items()])
-        scores = evaluator.evaluate(results)
+        scores = evaluate(qrels, results, measures)
 
-        metrics = {_k:0.0 for _k,__ in scores[list(scores.keys())[0]].items()}
-
-        for _,query_scores in scores.items():
-            for score_name,score in query_scores.items():
-                metrics[score_name] += score
-
-        for k,v in metrics.items():
-            metrics[k] = round(v/len(scores),5)
-
-        return metrics
+        return scores
 
 
-def evaluate(qrels_path: str, run_path: str, metrics_str: List[str]):
+def main(qrels_path: str, run_path: str, metrics: List[str]):
     """
     qrels_path and run_path should have the following format:
         {
@@ -51,29 +40,6 @@ def evaluate(qrels_path: str, run_path: str, metrics_str: List[str]):
     with open(run_path, "r") as f:
         run = json.load(f)
 
-    metrics = {}
-    for m in metrics_str:
-        parts = m.split("@")
-        if len(parts) == 2:
-            name = ""
-            ks = None
-
-            try:
-                name, ks = parts[0].lower(), parts[1]
-            except:
-                print(f"Invalid format for metric: {m}")
-
-            if name in PYTREC_METRIC_MAPPING:
-                _metric = PYTREC_METRIC_MAPPING[name]+"."+ks
-                metrics[name] = _metric
-
-        elif len(parts) == 1:
-            name = m.lower()
-
-            if name in PYTREC_METRIC_MAPPING:
-                _metric = PYTREC_METRIC_MAPPING[name]
-                metrics[name] = _metric
-
     scores = _evaluate(qrels, run, metrics)
     formatted_scores = {k.replace("_","@"): v for k,v in scores.items()}
     print(formatted_scores)
@@ -87,4 +53,4 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--metrics", default=DEFAULT_METRICS, type=str, nargs='+', required=False)
     args = parser.parse_args()
     
-    evaluate(qrels_path=args.qrels, run_path=args.run, metrics_str=args.metrics)
+    main(qrels_path=args.qrels, run_path=args.run, metrics=args.metrics)
